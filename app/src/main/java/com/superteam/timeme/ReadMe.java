e4a19e453cd284baf4f3ce9bc4d90105e39778a4
Rivka Shtarkman <rvksites@gmail.com>
2018-03-13 23:03:05 +0200

    adding the ReadMe file

---
+++ app/src/main/java/com/superteam/timeme/ReadMe.java
@@ -0,0 +1,13 @@
+package com.superteam.timeme;
+
+/**
+ * Created by User on 3/13/2018.
+ */
+
+public class ReadMe {
+    /*
+    Notes for the TimeMe project
+    ==============================
+
+     */
+}
